6e5534eae08a9e6a7c5ea0cd09b095d02da6434c
socratesssss <studentidsijan@gmail.com>
2025-08-10 16:31:34 +0600

    ff

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config({path:'.env'})
 const app = require('./App');
-const port = process.env.PORT || '5000';
+
 
 const DB = process.env.MONGODB_SERVER.replace('<PASSWORD>',process.env.DB_PASSWORD)
 
@@ -22,6 +22,6 @@ mongoose.connect(DB)
   });
 
 
-    app.listen(port, () => {
-      console.log(`🚀 Server running on${port}`);
+    app.listen( process.env.PORT, () => {
+      console.log(`🚀 Server running on${ process.env.PORT}`);
     });
